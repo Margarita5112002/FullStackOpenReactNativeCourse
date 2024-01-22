d1b6f680d1b3b449e06590d6bbbc9fd1c1a46895
Margarita <margaritaalcarazrojas@gmail.com>
2024-01-21 19:25:25 -0700

    Implement validation for sign in form - Exercise 10.9

--- src/components/FormikInputText.jsx
+++ src/components/FormikInputText.jsx
@@ -1,22 +1,35 @@
 import { StyleSheet } from 'react-native';
 import { useField } from 'formik';
-
+import theme from '../theme';
 import TextInput from './TextInput';
 import Text from './Text';
 
 const styles = StyleSheet.create({
   errorText: {
     marginTop: 5,
+    color: theme.colors.error
   },
+  textInput: {
+    borderWidth: 1
+  },
+  errorInput: {
+    borderColor: theme.colors.error
+  }
 });
 
 const FormikTextInput = ({ name, ...props }) => {
   const [field, meta, helpers] = useField(name);
   const showError = meta.touched && meta.error;
 
+  const inputStyle = [
+    styles.textInput,
+    showError && styles.errorInput
+  ]
+
   return (
     <>
       <TextInput
+        style={inputStyle}
         onChangeText={value => helpers.setValue(value)}
         onBlur={() => helpers.setTouched(true)}
         value={field.value}
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import { Formik } from "formik";
+import * as yup from 'yup';
 import { View, Pressable } from "react-native";
 import Text from "./Text";
 import FormikInputText from "./FormikInputText";
@@ -7,6 +8,11 @@ const initialValues = {
   user: "",
   password: "",
 };
+
+const validationSchema = yup.object().shape({
+    user: yup.string().required('user is required'),
+    password: yup.string().required('password is required')
+})
 
 const SignInForm = ({ onSubmit }) => {
   return (
@@ -24,7 +30,10 @@ const SignIn = () => {
   const onSubmit = values => {
     console.log(values)
   }
-  return <Formik onSubmit={onSubmit} initialValues={initialValues}>
+  return <Formik 
+    onSubmit={onSubmit} 
+    initialValues={initialValues}
+    validationSchema={validationSchema}>
     {({handleSubmit}) => <SignInForm onSubmit={handleSubmit}/>}
   </Formik>;
 };
